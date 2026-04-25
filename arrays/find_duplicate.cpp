c7b1dd1d5f9dbcf5cb5eb6e6a933825d19151455
psh-infra <priyanshu911x@gmail.com>
2026-04-25 23:21:34 +0530

    basic implementation

--- arrays/find_duplicate.cpp
+++ arrays/find_duplicate.cpp
@@ -1,19 +1,21 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
     int n;
     cin>>n;
-
     vector<int> arr(n);
     for(int i=0;i<n;i++) {
         cin>>arr[i];
     }
+    // find duplicate
     unordered_map<int,int> mp;
     for(int i=0;i<arr.size();i++) {
         mp[arr[i]]++;
+   }
+   for(auto it : mp) {
+    if(it.second>1) {
+        cout<<it.first<<endl;
     }
-    for(auto it:mp) {
-        cout<<it.first<<" "<<it.second<<endl;    
-    }
+   }
+    
 }
